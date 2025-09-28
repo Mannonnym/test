6c0fcfa37b6e13b4c0a4bc074d8f898c38f71d2d
anonnonym00 <saidasma832@gmail.com>
2025-09-28 19:56:08 +0200

    smthn

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar"
 import RateLimitedUI from "../components/RateLimitedUI"
+import axios from "axios"
 const Homepage=()=>{
     const [isRateLimited,setisRateLimited]=useState(true)
     const [Notes,setNotes]=useState([])
     const [Loading,setLoading]=useState(true)
+    useEffect(()=>{
+      const fetshNotes=async ()=>{
+        try {
+        const res=await axios.get("http://localhost:5000/api/notes")
+        console.log(res.data)
+        setNotes(res.data)
+        setLoading(false)
+
+      } catch (error) {
+        console.log("error")
+      }
+      }
+      
+  fetshNotes();
+
+
+    },[])
 return (
   <div className="min-h-screen">
     <Navbar />
